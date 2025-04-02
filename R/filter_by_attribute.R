42024006969cf12c8261ae895e194c2258de94d6
Andrea Manica <am315@cam.ac.uk>
2025-04-02 21:17:35 +0100

    printing

--- R/filter_by_attribute.R
+++ R/filter_by_attribute.R
@@ -1,6 +1,6 @@
 #' Filter the tracks based on variables from the metadata
 #'
-#' @param x A move2 object
+#' @param .data A move2 object
 #' @param ... The identifiers of one or more tracks to select or selection
 #' criteria based on track metadata
 #' @param .track_id A vector of the ids of the tracks to select
---
+++ R/print_move2.R
@@ -0,0 +1,51 @@
+#' @export
+print.move2 <- function(x, ..., n = getOption("sf_max_print", default = 10L)) {
+  avg_dur <- mean(do.call(c, lapply(lapply(split(move2::mt_time(x), move2::mt_track_id(x), drop = TRUE), range), diff)))
+  avg_dur <- lubridate::make_difftime(as.numeric(avg_dur, units = "secs"))
+  cat(cli::format_message(
+    "A {.cls move2} with `track_id_column` {.val {move2::mt_track_id_column(x)}} and `time_column` {.val {move2::mt_time_column(x)}}"
+  ), "\n", sep = "")
+  cat(cli::format_message("Containing {mt_n_tracks(x)} track{?s} lasting {?on average} {format(avg_dur, digits=3)} in a"), "\n", sep = "")
+  NextMethod(n = n)
+  message("To see track meta, use `mt_show_meta()`")
+  invisible(x)
+}
+
+#' Show the track metadata of a `move2` object
+#'
+#' @param x A move2 object
+#' @param ... Additional arguments passed to `print()`
+#' @param n The number of tracks to show metadata for
+#' @return The input `move2` object
+#' @export
+
+mt_show_meta <- function(x, ..., n = getOption("sf_max_print", default = 10L)) {
+  avg_dur <- mean(do.call(c, lapply(lapply(split(move2::mt_time(x), move2::mt_track_id(x), drop = TRUE), range), diff)))
+  avg_dur <- lubridate::make_difftime(as.numeric(avg_dur, units = "secs"))
+  cat(cli::format_message(
+    "A {.cls move2} with `track_id_column` {.val {move2::mt_track_id_column(x)}} and `time_column` {.val {move2::mt_time_column(x)}}"
+  ), "\n", sep = "")
+  cat(cli::format_message("Containing {mt_n_tracks(x)} track{?s} lasting {?on average} {format(avg_dur, digits=3)} in a"), "\n", sep = "")
+  # Print individual data
+  track_meta <- move2::mt_track_data(x)
+  if (n > 0L) {
+    if (inherits(track_meta, "tbl_df")) {
+      if (nrow(track_meta) > n) {
+        cat(paste("Metadata for the first ", n, "tracks:\n"))
+      } else {
+        cat(paste("Track metadata:\n"))
+      }
+      print(track_meta, ..., n = n)
+    } else {
+      y <- track_meta
+      if (nrow(y) > n) {
+        cat(paste("Metadata for the first ", n, "tracks:\n"))
+        y <- track_meta[1L:n, , drop = FALSE]
+      } else {
+        cat(paste("Track metadata:\n"))
+      }
+      print.data.frame(y, ...)
+    }
+  }
+  invisible(x)
+}
