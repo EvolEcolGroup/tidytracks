66e8891055b9927227d0aaa2013e5e57c55574b9
Andrea Manica <am315@cam.ac.uk>
2025-04-08 19:18:25 +0100

    split trip initial

--- R/fast_distance.R
+++ R/fast_distance.R
@@ -1,8 +1,23 @@
-fast_distance <- function (x1, y1, x2, y2){
-    if (longlat) {
-      geodist::geodist(cbind(lon = c(x1, tail(x2, 1)), lat = c(y1, tail(y2, 1))),
-                       sequential = FALSE, paired = TRUE, measure = "geodesic")/1000
-    } else sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+#' @keywords internal
+distance_fast <- function(x1, y1, x2, y2, longlat=TRUE) {
+  if (missing(y1)) {
+    if (!is.matrix(x1))
+      stop("x1 is not a matrix and multiple arguments not specified")
+    if (nrow(x1) < 2) stop("x1 has too few rows")
+    if (ncol(x1) < 2) stop("x1 has too few columns")
+    x2 <- x1[-1, 1]
+    y1 <- x1[-nrow(x1), 2]
+    y2 <- x1[-1, 2]
+    x1 <- x1[-nrow(x1), 1]
   }
-
+  nx <- length(x1)
+  if (nx != length(y1) | nx != length(x2) | nx != length(y2))
+    stop("arguments must have equal lengths")
+  if (longlat) {
+    geodist::geodist_vec(x1= x1, y = y1, x2 = x2, y2 = y2,
+                         sequential = FALSE, paired = TRUE, measure = "geodesic")
+  } else {
+    sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+  }
+}
 
--- R/mt_flag_mcconnell.R
+++ R/mt_flag_mcconnell.R
@@ -146,27 +146,3 @@ event_flag_mcconnell <- function (x, max_speed=NULL) {
 
   okFULL
 }
-
-
-distance_fast <- function(x1, y1, x2, y2, longlat=TRUE) {
-  if (missing(y1)) {
-    if (!is.matrix(x1))
-      stop("x1 is not a matrix and multiple arguments not specified")
-    if (nrow(x1) < 2) stop("x1 has too few rows")
-    if (ncol(x1) < 2) stop("x1 has too few columns")
-    x2 <- x1[-1, 1]
-    y1 <- x1[-nrow(x1), 2]
-    y2 <- x1[-1, 2]
-    x1 <- x1[-nrow(x1), 1]
-  }
-  nx <- length(x1)
-  if (nx != length(y1) | nx != length(x2) | nx != length(y2))
-    stop("arguments must have equal lengths")
-  if (longlat) {
-    geodist::geodist_vec(x1= x1, y = y1, x2 = x2, y2 = y2,
-                     sequential = FALSE, paired = TRUE, measure = "geodesic")
-  } else {
-    sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
-  }
-}
-
