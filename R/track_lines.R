09874f8e29731a6ccb0ef251ea55cc6bca0f55ac
Andrea Manica <am315@cam.ac.uk>
2025-04-10 08:28:13 +0100

    further sanitise function names

---
+++ R/track_lines.R
@@ -0,0 +1,18 @@
+#' Return a trajectory line for each track
+#'
+#' @description
+#' Converts each track into one line
+#'
+#' @param x A move object
+#'
+#' @param ... Arguments passed on to the [dplyr::summarise()] function
+#'
+#' @return A [sf::sf] object with a `LINESTRING` representing the track as geometry for each track. The
+#' metadata for each track is included as well as the products from summarize
+#'
+#' @details
+#' Note that all empty points are removed before summarizing. Arguments passed with `...` thus only summarize for the
+#' non empty locations.
+#' @export
+
+track_lines <- move2::mt_track_lines
