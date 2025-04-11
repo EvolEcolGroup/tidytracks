ffa6ffa4f57dc183792d700ee5799a6def3b6f7d
Andrea Manica <am315@cam.ac.uk>
2025-04-11 07:06:28 +0100

    more split trip

--- R/mt_split_trips.R
+++ R/mt_split_trips.R
@@ -15,10 +15,11 @@
 #' specified as a unit object, e.g `as_units(10000, "m")` or `as_units(10, "km")`.
 #' @param complete boolean, if TRUE, only complete trips (i.e. the ones that
 #' ended within the inbound buffer) are kept. If FALSE, all trips are kept, and
-#' events at the colony (i.e. inbetween trips) are collected into trip
+#' events at the colony (i.e. in-between trips) are collected into a dummy trip
 #' labelled "trip_na".
 #' @returns a move2 object with the trips split.
 #' @export
+#' @importFrom foreach %do%
 
 mt_split_trips <- function(x, center_col = NULL,
                            buffer_outbound = units::as_units(1000, "m"),
@@ -87,7 +88,7 @@ mt_split_trips <- function(x, center_col = NULL,
   unique_ids <- unique(ids)
 
   # Loop through each track and split into trips
-  trip_ids <- sapply(seq_len(nrow(mt_show_meta(x))), function(i) {
+  trip_list <- foreach::foreach(i = seq_len(nrow(mt_show_meta(x)))) %do% {
     split_one_track(unique_ids[i],
       coords[ids == unique_ids[i], 1],
       coords[ids == unique_ids[i], 2],
@@ -97,7 +98,14 @@ mt_split_trips <- function(x, center_col = NULL,
       buffer_inbound = buffer_in_uless,
       buffer_outbound = buffer_out_uless
     )
-  })
+  }
+
+  # Combine trip IDs into a single vector
+  x <- trip_ids <- unlist(purrr::map_depth(trip_list, 1, "trip_labels"))
+  # now get the trip meta and update the track meta accordingly
+  trip_meta <- purrr::map_depth(trip_list, 1, "trip_meta") %>% dplyr::bind_rows()
+  #@TODO
+
 
   # Update metadata with trip IDs
   metadata$trip_id <- trip_ids
@@ -141,38 +149,30 @@ split_one_track <- function(label,
   dist_to_center <- dist_fast(x, y, center_x_vec, center_y_vec, longlat = is_lonlat)
   # define distances outside the outbound buffer
   out_events <- dist_to_center > buffer_outbound
-  # define the runs of TRUE events
-  #  out_events <- c(T,T,T,F,F,T,T,T,F)
   # label trips
   runs <- rle(out_events)
   # now we can label the trips
   trip_labels <- rep(paste0(label, "trip_na"), length(runs$values))
   trip_labels[(runs$values) == TRUE] <-
     paste0(paste0(label, "_trip_"), seq_len(sum((runs$values) == TRUE)))
-  trip_labels <- rep(trip_labels, times = runs$lengths)
+  trip_labels_vec <- rep(trip_labels, times = runs$lengths)
 
-  browser()
-  # @TODO mark tracks as complete or not, or na trips
-  # we need to return a list with the trip labels and the trip status
+  # create the metadata for the labels
+  trip_meta <- tibble(
+    track_id = rep(label, length(unique(trip_labels))),
+    trip_id = unique(trip_labels),
+    trip_type = ifelse(
+      unique(trip_labels) != paste0(label, "_trip_"),
+      "complete", "at_center"
+    )
+  )
+  # check if last trip is incomplete
+  if (dist_to_center[nrow(trip_meta)] > buffer_inbound) {
+    trip_meta$trip_type[nrow(trip_meta)] <- "incomplete"
+  }
 
-  # check if the last event is within the inbound buffer
-  end_of_trips <- cumsum(runs$lengths)[runs$values == TRUE]
-  in_events <- dist_to_center[end_of_trips] < buffer_inbound
-  # now create a table to mark the trips as complete or not
-  browser()
-
-
-
-  # browser()
-  # # check if end of trips are within the inbound buffer
-  # last_dists <- dist_fast(x[end_of_trips], y[end_of_trips], center_x, center_y,
-  #                                longlat = is_lonlat)
-  # # check if the last event is within the inbound buffer
-  # last_dists <- last_dists < buffer_inbound
-  # # now set runs that did not pass the test to FALSE
-  # runs$values[runs$values==TRUE & last_dists==FALSE] <- FALSE
-
-
-
-  return(trip_labels)
+  return(list(
+    trip_labels = trip_labels_vec,
+    trip_meta = trip_meta
+  ))
 }
--- tests/testthat/test_mt_split_trips.R
+++ tests/testthat/test_mt_split_trips.R
@@ -1,19 +1,56 @@
+test_that("mt_trip_split works as expected", {
 # create a simple trajectory that goes out and comes back along the longitude axis
 set.seed(1)
-coords <- data.frame (longitude=c(0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 1, -2, -3,
-                                  -4, -5, -6, -4, -3, -1.9, 3, 4, 3.1),
-                      latitude=rnorm(24, mean=0, sd=0.1) +
-                        c(rep(0,6), rep(0.3, 5), rep(0.15,6), rep(-0.3, 6)))
-coords$date_time <- seq(from=as.POSIXct("2020-01-01 00:00:00"),
-                          by="1 hour", length.out=nrow(coords))
-coords$track_id <- rep(1, nrow(coords))
-coords$track_id <- as.factor(coords$track_id)
-coords$deployment_id <- coords$track_id
-coords$lon_colony <- 0
-coords$lat_colony <- 0
-coords <- sf::st_as_sf(coords, coords = c("longitude", "latitude"),
-                            crs = "+proj=longlat +datum=WGS84")
-test_mt <- move2::mt_as_move2(coords, track_id_column = "track_id",
+coords_df <- data.frame (longitude=c(0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0.5, -2, -3,
+                                  -4, -5, -3 -1.2, -0.3, -0.1, 1, 3, 4, 2.1,
+                                  1, 2, 3, 4, 3, 1.3), # second individual
+                      latitude=rnorm(30, mean=0, sd=0.1) +
+                        c(rep(0,6), rep(0.3, 5), rep(0.15,7), rep(-0.3, 6),
+                          rep(0.1,6)))
+coords_df$date_time <- c(seq(from=as.POSIXct("2020-01-01 00:00:00"),
+                          by="1 hour", length.out=24),
+                      seq(from=as.POSIXct("2020-02-10 00:00:00"),
+                          by="1 hour", length.out=6))
+coords_df$track_id <- paste0("id_",c(rep(1, 24), rep(2,6)))
+coords_df$track_id <- as.factor(coords_df$track_id)
+coords_df$deployment_id <- coords_df$track_id
+coords_df$lon_colony <- 0
+coords_df$lat_colony <- 0
+coords_df <- sf::st_as_sf(coords_df, coords = c("longitude", "latitude"),
+                            crs = "+proj=longlat +datum=WGS84") # todo use equal area proj in km
+test_mt <- move2::mt_as_move2(coords_df, track_id_column = "track_id",
                             time_column = "date_time")
 ggplot(test_mt) + geom_sf(aes (color = date_time))+
   geom_sf(data = move2::mt_track_lines(test_mt))
+# add some metadata
+center_sf <- sf::st_as_sf(
+  data.frame(
+    lon = rep(0, 1),
+    lat = rep(0, 1)
+  ),
+  coords = c("lon", "lat"), crs = "+proj=longlat +datum=WGS84"
+)
+
+# now test the trip splitting
+
+  # measure distances from point 0,0
+  coords <- sf::st_coordinates(test_mt)
+  center_dist <- geodist::geodist(x= coords,
+                                  y=data.frame(lon=0, lat=0),
+                                  measure="geodesic")/1000
+  # points at the colony
+  at_center <- center_dist < 100
+
+  # test the trip splitting
+  test_mt_split <- mt_split_trips(test_mt, center_col = center_sf,
+                                  buffer_outbound = as_units(100, "km"),
+                                  buffer_inbound = as_units(100,"km"))
+  # check that the trip ids are correct
+  expect_equal(length(unique(test_mt_split$trip_id)), 6)
+  expect_equal(length(unique(test_mt_split$track_id)), 1)
+  expect_equal(length(unique(test_mt_split$event_id)), nrow(coords))
+  expect_equal(length(unique(test_mt_split$trip_id[!is.na(test_mt_split$trip_id)])), 6)
+
+
+
+})
