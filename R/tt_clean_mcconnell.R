d5a285c8f02f33884c0e6b0e4e3f863d877ef75f
Andrea Manica <am315@cam.ac.uk>
2025-04-29 21:08:22 +0100

    multiple outputs from tt_clean_mcconnell
    tests are not finished

--- R/tt_clean_mcconnell.R
+++ R/tt_clean_mcconnell.R
@@ -9,17 +9,28 @@
 #' @param x A move2 object
 #' @param max_speed speed, provided as a `units` object (e.g.
 #' units::as_units(50, 'm/s').
+#' @param flag_action One of "remove", "null", or "interpolate", to either
+#' remove the points flagged by the algorithm, keep the time stamp but set
+#' location to NULL, or replace the location with a linearly interpolated
+#' location from the events that were not filtered out.
 #' @return A clean `move2` object with events removed
 #' @export
 
-tt_clean_mcconnell <- function(x, max_speed = NULL) {
+tt_clean_mcconnell <- function(x, max_speed = NULL,
+                               flag_action = c("remove", "interpolate", "null")) {
   # checking for appropriate x and max_speed is done by event_flag_mcconnell
+  flag_action <- match.arg(flag_action)
 
   # Call the event_flag_mcconnell function to get the valid points
   valid_points <- event_flag_mcconnell(x, max_speed)
 
-  # Filter the move2 object based on the valid points
-  x_clean <- x[valid_points, ]
-
-  return(x_clean)
+  if (flag_action=="remove"){
+    x <- x[valid_points,]
+  } else if (flag_action=="null"){
+    sf::st_geometry(x)[!valid_points] <- sf::st_point()
+  } else if (flag_action == "interpolate"){
+    sf::st_geometry(x)[!valid_points] <- sf::st_point()
+    x <- move2::mt_interpolate(x)
+  }
+  return(x)
 }
--- tests/testthat/test_event_flag_mcconnell.R
+++ tests/testthat/test_event_flag_mcconnell.R
@@ -1,19 +1,33 @@
+walrus_sub <- trip::walrus818[1:600, ]
+# create a walrus dataset
+walrus_mt <- mt_as_move2(walrus_sub)
+
 test_that("event_flag_mcconnell works correctly", {
-  # create a walrus dataset
-  walrus_mt <- mt_as_move2(trip::walrus818[1:600, ])
-  walrus_cleaned <- event_flag_mcconnell(walrus_mt,
-                                        max_speed = units::as_units(1000, "m/h")
-  )
+  walrus_cleaned <-
+    event_flag_mcconnell(walrus_mt,
+                         max_speed = as_units(1000, "m/h"))
   # compare the output of event_flag_mcconnell with the output of speedfilter
   # it works for projected distances, but it will not for longlat
   expect_true(all.equal(
-    trip::speedfilter(trip::walrus818[1:600, ], max.speed = 1000),
+    trip::speedfilter(walrus_sub, max.speed = 1000),
     walrus_cleaned
   ))
   # check that units are handle correctly
   expect_true(all.equal(
     event_flag_mcconnell(mt_as_move2(trip::walrus818[1:600, ]),
-                         max_speed = units::as_units(1, "km/h")
+                         max_speed = as_units(1, "km/h")
     ),
     walrus_cleaned))
+})
+
+test_that("tt_clean_mcconnel correctly handles filtered data",{
+  # first check that setting to null works
+  walrus_clean_null <- tt_clean_mcconnell(walrus_mt,
+                                          max_speed = as_units(1000, "m/h"),
+                                          flag_action = "null")
+  walrus_clean_rm <- tt_clean_mcconnell(walrus_mt,
+                                        max_speed = as_units(1000, "m/h"),
+                                        flag_action = "remove")
+  # number of rows in rm is equal to all rows minus the null
+  nrow(walrus_clean_null)- sum(is.na(sf::st_coordinates(walrus_clean_null))[,1])
 })
