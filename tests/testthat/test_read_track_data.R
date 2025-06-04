fc7e794f5cc9e31c0b1f935aad455918b0bdfa6a
Andrea Manica <am315@cam.ac.uk>
2025-06-04 08:28:17 +0100

    example read of csv

--- tests/testthat/test_read_track_data.R
+++ tests/testthat/test_read_track_data.R
@@ -1,13 +1,19 @@
 test_that("tt_read_track_data works with valid input", {
-  # Create a temporary file with valid track data
-  read.csv("testdata/csv_files/dataset_example_birdlife.csv")
+  # Read in the data from a csv
+  example_csv <- read.csv(test_path("testdata/csv_files/dataset_example_birdlife.csv"))
+  # convert it to an sf object
+  example_sf <- sf::st_as_sf(example_csv, coords = c("longitude", "latitude"), crs = 4326)
+  # create a date-time column
+  example_sf <- example_sf %>%
+    mutate(date_time = lubridate::ymd_hms(paste(date_gmt, time_gmt))) %>%
+    select(-c(date_gmt, time_gmt))
+  # convert it to a move2 object
+  example_mt <- move2::mt_as_move2(example_sf,
+                               time_column = "date_time",
+                               track_id_column = "track_id",
+                               track_attributes = c("site_name", "colony_name",
+                                                    "lat_colony", "lon_colony",
+                                                    "device","bird_id", "age",
+                                                    "sex", "breed_state", "breed_status"))
 
-  # Call the function and check the output
-  result <- read_track_data(temp_file)
-  expect_true(is.data.frame(result))
-  expect_equal(ncol(result), 3)
-  expect_equal(nrow(result), 10)
-
-  # Clean up
-  unlink(temp_file)
 })
