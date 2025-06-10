2e5a1775aab0503a75bcfbd2aa780fa2282bbb31
Andrea Manica <am315@cam.ac.uk>
2025-06-10 14:49:41 +0100

    add tt_read_events with vignette

--- tests/testthat/test_tt_read_events.R
+++ tests/testthat/test_tt_read_events.R
@@ -4,21 +4,22 @@ test_that("tt_read_track_data works with valid input", {
   example_csv <- system.file("/extdata/csv_files/dataset_example_birdlife.csv",
                                      package = "tidytracks")
   
+  example_tt <- tt_read_events(albatross_csv,
+                               col_track_id = "track_id",
+                               col_coords = c("longitude", "latitude"),
+                               col_date_time = c("date_gmt", "time_gmt"))
   
+  # Check that the move2 object is created correctly
+  expect_s3_class(example_tt, "move2")
+  # only have date and locations as event specific variables
+  expect_true(length(names(example_tt)) == 3)
+  expect_true(all(c("track_id", "date_time", "geometry") %in% names(example_tt)))
+  # Check that the meta data is correctly populated
+  # expect that the meta data has the correct columns
+  expect_true(
+    all(
+      c("scientific_name", "age", "breed_status") %in%
+        names(show_meta(example_tt))))
   
-  # convert it to an sf object
-  example_sf <- sf::st_as_sf(example_csv, coords = c("longitude", "latitude"), crs = 4326)
-  # create a date-time column
-  example_sf <- example_sf %>%
-    mutate(date_time = lubridate::ymd_hms(paste(date_gmt, time_gmt))) %>%
-    select(-c(date_gmt, time_gmt))
-  # convert it to a move2 object
-  example_mt <- move2::mt_as_move2(example_sf,
-                               time_column = "date_time",
-                               track_id_column = "track_id",
-                               track_attributes = c("site_name", "colony_name",
-                                                    "lat_colony", "lon_colony",
-                                                    "device","bird_id", "age",
-                                                    "sex", "breed_state", "breed_status"))
-
+  #@TODO we need tests for bring in a meta.csv, as well as various errors
 })
