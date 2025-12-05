e1d559d48593e220a7677954bb175904c7e6d0bc
Andrea Manica <am315@cam.ac.uk>
2025-12-05 12:31:43 +0000

    fix one check warning

--- R/geom_event_path.R
+++ R/geom_event_path.R
@@ -51,8 +51,8 @@ geom_event_path <- function(mapping = ggplot2::aes(), data = NULL, stat = "sf",
     dplyr::mutate(geom_steps = move2::mt_segments(data))
   # change the geometry column
   data_steps <- data_steps %>%
-    dplyr::mutate(geometry = geom_steps) %>%
-    dplyr::select(-geom_steps)
+    dplyr::mutate(geometry = .data$geom_steps) %>%
+    dplyr::select(-dplyr::all_of("geom_steps"))
   # drop all units
   data_steps <- tt_drop_units(data_steps)
 
