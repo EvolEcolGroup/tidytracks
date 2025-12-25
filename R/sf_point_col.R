5cb249a29f04f6d0e101a5fa126462cff1bda8ea
Andrea Manica <am315@cam.ac.uk>
2025-12-25 18:33:48 +0000

    create geometry set column

---
+++ R/sf_point_col.R
@@ -0,0 +1,25 @@
+#' Create a simple feature POINT geometry column
+#' 
+#' This function creates a simple feature POINT geometry column from
+#' x and y columns in a data frame. The resulting object is
+#' a `sf` geometry set that can be used as an additional spatial column
+#' in an sf data frame, in addition to the primary geometry column.
+#'
+#' @param x A vector of x coordinates (e.g., longitude or easting)
+#' @param y A vector of y coordinates (e.g., latitude or northing)
+#' @param crs An optional coordinate reference system (CRS) to assign to
+#' the geometry. This can be an EPSG code (numeric) or a proj4 string (character).
+#' If NULL (default), no CRS is assigned.
+#' @return A `sf` geometry set of POINT geometries
+#' @export
+#' 
+sf_point_col <- function(x, y, crs = NULL) {
+  sf::st_sfc(
+    mapply(
+      function(x, y) sf::st_point(c(x, y)),
+      x, y,
+      SIMPLIFY = FALSE
+    ),
+    crs = crs
+  )
+}
