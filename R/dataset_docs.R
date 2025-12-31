fb27dcad0bd6135c9998bc40dd1bbb4fec81a7ff
Andrea Manica <am315@cam.ac.uk>
2025-12-31 13:56:57 +0000

    implement event_interval

--- R/dataset_docs.R
+++ R/dataset_docs.R
@@ -15,3 +15,18 @@
 #'   for each event}
 #' }
 "shags_tt"
+
+
+#' A simple example move2 object
+#' A very simple dataset of 3 individuals, with 5 observations per individual,
+#' used for examples in the package documentation.
+#'  
+#' @format An move2 object with 15 events from 3 tracks (one per individual).
+#' We have 3 columns
+#' \describe{
+#'   \item{track_id}{ids of each track}
+#'   \item{date_time}{time stamp for each event}
+#'   \item{geometry}{longitudes and latitudes, as an `sf` geometry
+#'   for each event}
+#' }
+"example_tt"
---
+++ data-raw/make_example_tt.R
@@ -0,0 +1,11 @@
+# make a very simple dataset of 3 individual, with 5 obs per individual,
+# used for examples
+
+tt_read_data("./data-raw/raw_csv/example_tt.csv",
+             col_track_id = "track_id",
+             col_coords = c("lon", "lat"),
+             col_date_time = "date_time",
+             format_date_time = "%Y-%m-%d %H:%M:%S",
+             time_zone = "UTC",
+             crs = 4326) -> example_tt
+usethis::use_data(example_tt, overwrite = TRUE)
