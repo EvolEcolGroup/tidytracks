b16cb12a6479fe5d02ce5e3f84de71fd5a951184
Holly R. W. Pickett <hrwp2@cam.ac.uk>
2026-01-19 16:48:16 +0000

    Neatened up examples of 2 specicic failures and added small example dataset

--- data-raw/kde_example_data.R
+++ data-raw/kde_example_data.R
@@ -1,69 +1,75 @@
-library(tidytracks)
 library(dplyr)
+library(sf)
+library(ggplot2)
+library(rnaturalearth)
+library(tidytracks)
 
-#read in data
-shag_data <- read.csv("inst/extdata/shag_tidytrack_sample.csv")
-
-# metadata for colony coordinates
-meta <- read.csv("inst/extdata/meta_shag_tidytrack.csv")
-
-#check column names and remove doubles so don't end up with duplicated in metadata
-colnames(shag_data)
-colnames(meta)
-
-meta <- meta %>%
-  dplyr::select(-c(sex, colony, year))
-
-#read in as TT object
-shags_mt <- tt_read_data(events = shag_data,
-                       col_track_id = "bird_id",
-                       col_coords = c("longitude", "latitude"),
-                       col_date_time = "datetime",
-                        meta= meta)
-
-#check
-head(shag_data)
-head(show_meta(shags_mt))
-
-#make sf point column for colony coordinates
-show_meta(shags_mt)$colony_coords <- 
-  sf_point_col(show_meta(shags_mt)$colony_lon,
-               show_meta(shags_mt)$colony_lat,
-               crs = 4326)
-  
-show_meta(shags_mt)
+# read in rds
+ten_tracks <- readRDS("data-raw/kde_example_data_bba.rds")
 
 # Reproject to equal area for KDEs
 
-shags_mt <- shags_mt %>%
+ten_tracks<- ten_tracks %>%
   sf::st_transform(crs = "+proj=laea +lat_0=-67 +lon_0=-68 +datum=WGS84 +units=m +no_defs")
 
-# Check CRS (todo- use CRSTools 'suggest projection function)
-sf::st_crs(shags_mt)
+# check associated metadata
+str(show_meta(ten_tracks))
 
-#plot tracks
-
-
-#create raw KDE and visualize grid
-shags_kde_1 <- shags_mt %>%
-  group_by(bird_id) %>%
-  tt_hr_kde(levels = NULL, 
-            h="h_ref_mean")
-
-#We can plot the first kde with:
-image(shags_kde$kde[[1]]$z)
-
-#we can see that the grid resolution is quite coarse
-
-shags_kde_2 <- shags_mt %>%
-  group_by(bird_id) %>%
+# functioning (if quite coarse) kde
+ten_tracks_kde <- ten_tracks %>%
+  group_by(track_id) %>%
   tt_hr_kde(levels = c(0.5, 0.95), 
-            h="h_ref_mean")
+            h="h_ref_mean",
+            res = 10000)
 
-#check attributes of the kde to see which bbox and res were used
-attributes(shags_kde_2)
+# Break 1: bounding box too small
 
-#-7825148 -3031784 15663818  5916394 
-#$res =  372840.7
+broken_bbox <- list(
+  xmin = 2.2e6,
+  ymin = 1.4e6,
+  xmax = 2.4e6,
+  ymax = 1.6e6
+)
 
+ten_tracks_kde_1 <- ten_tracks %>%
+  group_by(track_id) %>%
+  tt_hr_kde(
+  levels = c(0.5, 0.95),
+  h = "h_ref_mean",
+  bbox = broken_bbox,
+  res = 10000
+)
+
+# causes error "polygons not (all) closed", and won't even compute, because bbox is too small 
+
+# Break 2: resolution too course
+
+# remove the massive track to make this easier to see
+
+nine_tracks <- ten_tracks %>%
+  filter(!track_id %in% c("82036"))
+
+# make kde with very coarse resolution
+
+nine_tracks_kde_1 <- nine_tracks %>%
+  group_by(track_id) %>%
+  tt_hr_kde(levels = c(0.5, 0.95), 
+            h="h_ref_mean",
+            res = 50000) 
+
+# count number of empty geometries
+sum(sf::st_is_empty(nine_tracks_kde_1))
+
+# try with finer res
+nine_tracks_kde_2 <- nine_tracks %>%
+  group_by(track_id) %>%
+  tt_hr_kde(levels = c(0.5, 0.95), 
+            h="h_ref_mean",
+            res = 20000) 
+
+# count number of empty geometries
+sum(sf::st_is_empty(nine_tracks_kde_2))
+
+# still one left, on really short track
+#so should maybe have warning here saying empty polygons created
 
