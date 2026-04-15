211b9c1569ef96aab41f53406feca36c8764a116
Lizzie Pearmain <ejp69@cam.ac.uk>
2026-04-15 16:52:05 +0100

    fix tt_animate tests

--- tests/testthat/test_tt_animate.R
+++ tests/testthat/test_tt_animate.R
@@ -44,7 +44,7 @@ test_that("tt_animate errors if plot_lims is wrong length or type", {
 test_that("n_frames and p_anim are correct with type = points", {
   # with extra frames prepended so the wake starts when the bird starts moving
   result <- tt_animate(x, type = "points", pad_start = TRUE)
-  expect_type(list_anim$n_frames, "integer")
+  expect_type(result$n_frames, "integer")
   expect_s3_class(result$p_anim, "gganim")
   # without the prepended extra frames
   result <- tt_animate(x, type = "points", pad_start = FALSE)
@@ -85,7 +85,7 @@ test_that("tt_animate embeds label_format in the title for type=paths", {
 })
 
 # for manual testing only: animate the gganim using av_renderer()
-gganimate::animate(plot = result$p_anim,
-                   n_frames = result$n_frames,
-                   fps = 10,
-                   renderer = gganimate::av_renderer())
+# gganimate::animate(plot = result$p_anim,
+#                    n_frames = result$n_frames,
+#                    fps = 10,
+#                    renderer = gganimate::av_renderer())
