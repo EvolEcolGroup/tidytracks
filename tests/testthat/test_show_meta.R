9e94159593f56a91ec01cda05acce27166c6e8bf
Lizzie Pearmain <ejp69@cam.ac.uk>
2026-06-12 16:08:50 +0100

    fix bug in filter_by_meta, add tests for filter_by_meta, update tests for show_meta that were erroring, and fix vignette so that it runs (but still needs updating).

--- tests/testthat/test_show_meta.R
+++ tests/testthat/test_show_meta.R
@@ -1,11 +1,12 @@
 test_that("show_meta works correctly", {
-  meta <- show_meta(shags_tt)
+  toy_tt <- create_toy_tt()
+  meta <- show_meta(toy_tt)
   expect_true(is.data.frame(meta))
-  expect_equal(nrow(meta), mt_n_tracks(shags_tt))
+  expect_equal(nrow(meta), mt_n_tracks(toy_tt))
 
   # Test setting metadata
-  show_meta(shags_tt)$test_column <- "test_info"
-  updated_meta <- show_meta(shags_tt)
+  show_meta(toy_tt)$test_column <- "test_info"
+  updated_meta <- show_meta(toy_tt)
   expect_true("test_column" %in% colnames(updated_meta))
   expect_equal(updated_meta$test_column[1], "test_info")
 })
