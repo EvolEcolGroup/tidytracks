44df68ebba0fb7d6bcc01a9cf6da824eb781d476
Andrea Manica <am315@cam.ac.uk>
2026-06-26 11:54:43 +0100

    fix as.data.frame

--- R/as_data_frame.R
+++ R/as_data_frame.R
@@ -8,29 +8,33 @@
 #' @param ...	additional arguments to be passed to or from methods.
 #' @param include_meta Logical, whether to include the metadata attributes in
 #'   the resulting data frame. Default is TRUE.
+#' @param drop_geometry Logical, whether to drop the geometry column from the
+#'  resulting data frame. Default is FALSE. If TRUE, the coordinates are stored
+#'  in separate columns (`X` and `Y`).
 #' @return A data frame with one row per event, including the event attributes,
 #'   track
 #' @export
 #' @rdname as_data_frame_move2
 #' @examples
 #' # example code
-#' as.data.frame(example_tt)
+#' as.data.frame(example_tt, include_meta = TRUE, drop_geometry = TRUE)
 
-as.data.frame.move2 <- function(x, ..., include_meta = TRUE) {
+as.data.frame.move2 <- function(x, ..., include_meta = FALSE, drop_geometry = FALSE) {
 
   # If include_meta is TRUE, join the metadata attributes to the data frame
   if (include_meta) {
     x <- x |> move2::mt_as_event_attribute(dplyr::any_of(names(show_meta(x))))
   }
-  # drop the geometry column and convert to a data frame
-  cbind(x |>
-    dplyr::select(-dplyr::all_of("geometry")) |>
-    NextMethod() |>   # Convert the move2 object to a data frame
-    dplyr::select(
-      -dplyr::all_of("geometry"),  # drop geom column
-    ),
-    sf::st_coordinates(x)) # add the coordinates as individual columns
-
-
-  #NextMethod()
+  
+  if (!drop_geometry) {
+    return(NextMethod())
+  } else {
+    
+    # drop the geometry column and convert to a data frame
+    cbind(x |>
+            NextMethod() |>   # Convert the move2 object to a data frame
+          dplyr::select(-dplyr::all_of("geometry")),
+          sf::st_coordinates(x$geometry)) # add the coordinates as individual columns
+    
+  }
 }
