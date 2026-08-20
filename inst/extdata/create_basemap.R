e29e790364baf5bc69628559f3323e8c8993a892
Holly Pickett - BAS <Holly.Pickett@bas.ac.uk>
2026-08-20 18:16:31 +0100

    update map for KDE extent

--- inst/extdata/create_basemap.R
+++ inst/extdata/create_basemap.R
@@ -7,19 +7,26 @@ library(ggplot2)
 
 
 # Get bbox of shags_tt (EPSG:4326) and turn it into an sf polygon
-shags_bbox <- st_as_sfc(st_bbox(shags_tt)) # is wgd84
+
+# having looked at KDEs expand again to - 67 and - 68 lat and- 67 and -68 lon
+# set bbox to this
+shags_bbox <- st_as_sfc(st_bbox(c(xmin = -68.5, xmax = -67, ymin = -68, ymax = -67), crs = st_crs(4326)))
+
 st_crs(shags_bbox)
 #  expand box by 0.5 degrees in all directions
 shags_bbox_expanded <- st_buffer(shags_bbox, dist = 0.5) %>% st_set_crs(4326)
 
+
 bmap <- basemap_terra(shags_bbox_expanded , map_service = "esri", map_type = "world_imagery")
 
 map <- ggplot() +
-  geom_spatraster_rgb(data = bmap_coarse) +   # basemap
+  geom_spatraster_rgb(data = bmap) +   # basemap
   # add in coastline
   geom_event_path(data = shags_tt, aes(col = bird_id), size = 2, # tracks
                   lineend = "round")  +            # track colours
   coord_sf(crs = 4326)        # projection
+
+map
 
 res(bmap)
 bmap_coarse <- aggregate(bmap, fact = 2, fun = "mean")
