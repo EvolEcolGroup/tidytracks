f0401a12e2e39b8ffb18b0679daf35223395094d
Lizzie Pearmain <ejp69@cam.ac.uk>
2026-09-24 15:22:07 +0100

    update track_duration to return values in original track order, add tests. Update filename for event_dist to event_distance to match function name.

---
+++ data-raw/make_example_tt_2.R
@@ -0,0 +1,18 @@
+# alternative version of example dataset with non-alphabetical track IDs and
+# differing durations, to test track_ functions.
+
+tt_read_data(
+  "./data-raw/raw_csv/example_tt_2.csv",
+  col_track_id = "track_id",
+  col_coords = c("lon", "lat"),
+  col_date_time = "date_time",
+  format_date_time = "%Y-%m-%d %H:%M:%S",
+  time_zone = "UTC",
+  crs = 4326
+) -> example_tt_2
+# add a metadata column
+show_meta(example_tt_2)$sex <- c("male", "male", "female")
+show_meta(example_tt_2)$nest_lon <- c(1.37, -2.44, -0.78)
+show_meta(example_tt_2)$nest_lat <- c(0.06, -1.76, 0.43)
+
+usethis::use_data(example_tt_2, overwrite = TRUE)
---
+++ tests/testthat/test_track_duration.R
@@ -0,0 +1,28 @@
+test_that("track_duration gives the correct errors", {
+  # try running it on an object that isn't a move2 object
+  expect_error(
+    track_duration(x = data.frame(a = 1:10, b = 11:20)),
+    "x must be a move2 object"
+  )
+})
+
+test_that("track_duration correctly computes track duration", {
+  expect_equal(
+    unname(track_duration(example_tt, units = "hours")[1]),
+    as_units(c(4/3), "hours"))
+  expect_equal(
+    length(track_duration(example_tt, units = "hours")),
+    3)
+})
+
+test_that("track_duration returns values in original order", {
+  # example_tt_2 has tracks that first appear in the order b, a, c, which
+  # differs from alphabetical order
+  durations <- track_duration(example_tt_2, units = "hours")
+
+  expect_equal(names(durations), c("b", "a", "c"))
+  expect_equal(
+    unname(durations),
+    as_units(c(5 / 3, 4 / 3, 1), "hours")
+  )
+})
